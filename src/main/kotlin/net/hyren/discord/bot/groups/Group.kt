40f979558a50abcb5b0609f4e3c6cfc7842badbe
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-04 07:04:49 -0300

    dev

--- src/main/kotlin/net/hyren/discord/bot/groups/Group.kt
+++ src/main/kotlin/net/hyren/discord/bot/groups/Group.kt
@@ -9,21 +9,21 @@ import net.hyren.discord.bot.DiscordBotConstants
  * @author Gutyerrez
  */
 fun Group.asRole(
-        guild: Guild? = DiscordBotConstants.GUILDS[DiscordBotConstants.GuildType.MAIN]
+    guild: Guild? = DiscordBotConstants.GUILDS[DiscordBotConstants.GuildType.MAIN]
 ): Role? {
     if (guild?.idLong == DiscordBotConstants.MAIN_GUILD_ID) {
         when {
-            this == Group.MASTER -> DiscordBotConstants.Roles.MAIN_GUILD.MASTER
-            this == Group.MANAGER -> DiscordBotConstants.Roles.MAIN_GUILD.MANAGER
-            this == Group.ADMINISTRATOR -> DiscordBotConstants.Roles.MAIN_GUILD.ADMINISTRATOR
-            this == Group.MODERATOR -> DiscordBotConstants.Roles.MAIN_GUILD.MODERATOR
-            this == Group.HELPER -> DiscordBotConstants.Roles.MAIN_GUILD.HELPER
-            this == Group.YOUTUBER -> DiscordBotConstants.Roles.MAIN_GUILD.YOUTUBER
-            this == Group.MVP_PLUS -> DiscordBotConstants.Roles.MAIN_GUILD.MVP_PLUS
-            this == Group.MVP -> DiscordBotConstants.Roles.MAIN_GUILD.MVP
-            this == Group.VIP_PLUS -> DiscordBotConstants.Roles.MAIN_GUILD.VIP_PLUS
-            this == Group.VIP -> DiscordBotConstants.Roles.MAIN_GUILD.VIP
-            this == Group.DEFAULT -> DiscordBotConstants.Roles.MAIN_GUILD.MEMBER
+            this == Group.MASTER -> DiscordBotConstants.Roles.MainGuild.MASTER
+            this == Group.MANAGER -> DiscordBotConstants.Roles.MainGuild.MANAGER
+            this == Group.ADMINISTRATOR -> DiscordBotConstants.Roles.MainGuild.ADMINISTRATOR
+            this == Group.MODERATOR -> DiscordBotConstants.Roles.MainGuild.MODERATOR
+            this == Group.HELPER -> DiscordBotConstants.Roles.MainGuild.HELPER
+            this == Group.YOUTUBER -> DiscordBotConstants.Roles.MainGuild.YOUTUBER
+            this == Group.MVP_PLUS -> DiscordBotConstants.Roles.MainGuild.MVP_PLUS
+            this == Group.MVP -> DiscordBotConstants.Roles.MainGuild.MVP
+            this == Group.VIP_PLUS -> DiscordBotConstants.Roles.MainGuild.VIP_PLUS
+            this == Group.VIP -> DiscordBotConstants.Roles.MainGuild.VIP
+            this == Group.DEFAULT -> DiscordBotConstants.Roles.MainGuild.MEMBER
         }
     }
 
