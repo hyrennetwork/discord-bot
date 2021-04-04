fdf2a974ef728ce8fbc34d4e5b71c88e095ec94d
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-04 06:25:49 -0300

    dev

--- src/main/kotlin/net/hyren/discord/bot/DiscordBotProvider.kt
+++ src/main/kotlin/net/hyren/discord/bot/DiscordBotProvider.kt
@@ -1,6 +1,9 @@
 package net.hyren.discord.bot
 
 import com.redefantasy.core.shared.CoreProvider
+import com.redefantasy.core.shared.applications.ApplicationType
+import com.redefantasy.core.shared.applications.data.Application
+import java.net.InetSocketAddress
 
 /**
  * @author Gutyerrez
@@ -9,8 +12,25 @@ object DiscordBotProvider {
 
     fun prepare() {
         CoreProvider.prepare(
-            10080
+                Application(
+                "discord-bot",
+                "Discord Bot",
+                null,
+                InetSocketAddress(
+                        "127.0.0.1",
+                        0
+                ),
+                ApplicationType.GENERIC,
+                null,
+                null
+            )
         )
+
+        val echo = CoreProvider.Databases.Redis.ECHO.provide()
+
+        echo.defaultSubscriber = echo.subscribe { _, runnable ->
+            runnable.run()
+        }
     }
 
 }
