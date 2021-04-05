c730cd0df4f194a7f2d2b712f63e26c48d9d17c1
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-04 22:52:55 -0300

    dev

--- src/main/kotlin/net/hyren/discord/bot/listener/adapter/GenericListeners.kt
+++ src/main/kotlin/net/hyren/discord/bot/listener/adapter/GenericListeners.kt
@@ -3,13 +3,11 @@ package net.hyren.discord.bot.listener.adapter
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
 import net.dv8tion.jda.api.events.guild.member.GuildMemberUpdateEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.events.user.UserActivityEndEvent
 import net.dv8tion.jda.api.events.user.UserActivityStartEvent
 import net.dv8tion.jda.api.events.user.UserTypingEvent
 import net.dv8tion.jda.api.events.user.update.UserUpdateOnlineStatusEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
-import net.hyren.discord.bot.DiscordBotApplication
 import net.hyren.discord.bot.DiscordBotConstants
 import net.hyren.discord.bot.misc.jda.member.canNotSyncData
 import net.hyren.discord.bot.misc.jda.member.removeRoles
@@ -91,27 +89,6 @@ class GenericListeners : ListenerAdapter() {
             } ?: return
 
             guild.addRoleToMember(member, verificationRole).queue()
-        }
-    }
-
-    override fun onGuildMessageReceived(
-        event: GuildMessageReceivedEvent
-    ) {
-        val member = event.member
-        val channel = event.channel
-
-        println("dale")
-
-        println(channel.idLong == 828128934032834580)
-        println(member == null)
-        println(member?.idLong != DiscordBotApplication.jda.selfUser.idLong)
-
-        if (channel.idLong == 828128934032834580 && member == null || member?.idLong != DiscordBotApplication.jda.selfUser.idLong) {
-            println("?")
-
-            val message = event.message
-
-            message.delete().queue()
         }
     }
 
