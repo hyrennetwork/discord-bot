0aa1d4f1350dc5dab0aa1b5145bec3674730ad5b
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-04-18 19:23:20 -0300

    dev

--- src/main/kotlin/net/hyren/discord/bot/echo/packet/listener/ProjectStartedBuildEchoPacketListener.kt
+++ src/main/kotlin/net/hyren/discord/bot/echo/packet/listener/ProjectStartedBuildEchoPacketListener.kt
@@ -3,7 +3,7 @@ package net.hyren.discord.bot.echo.packet.listener
 import com.redefantasy.core.shared.echo.api.listener.EchoListener
 import com.redefantasy.core.shared.echo.packets.project.ProjectStartedBuildEchoPacket
 import net.dv8tion.jda.api.EmbedBuilder
-import net.hyren.discord.bot.DiscordBotConstants
+import net.hyren.discord.bot.DiscordBotApplication
 import org.greenrobot.eventbus.Subscribe
 
 /**
@@ -17,9 +17,11 @@ class ProjectStartedBuildEchoPacketListener : EchoListener {
 	) {
 		println("AA")
 
-		println(DiscordBotConstants.Channels.GENERAL_CHAT === null)
+		val chat = DiscordBotApplication.jda.getTextChannelById(826289489214701603)
 
-		DiscordBotConstants.Channels.GENERAL_CHAT?.sendMessage(
+		println(chat === null)
+
+		chat?.sendMessage(
 			EmbedBuilder()
 				.setTitle("Iniciando build #${packet.buildId}")
 				.addField(
